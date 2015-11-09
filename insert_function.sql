4a598028625af10122192af4dd217d771f995a8d
rad <raphael.a.dumas@gmail.com>
2015-11-09 17:36:11 -0500

    Beginning to modify crowdmapping functionality for bikeways

--- insert_function.sql
+++ insert_function.sql
@@ -1,10 +1,11 @@
-DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text);
+DROP FUNCTION IF EXISTS insert_bikeways_data(text,text,text,numeric);
 --Assumes only one value being inserted
 
-CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
+CREATE OR REPLACE FUNCTION insert_bikeways_data (
     _geojson TEXT,
-    _description TEXT,
-    _name TEXT)
+    _notes TEXT,
+    _name TEXT,
+    _zip numeric)
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
@@ -14,9 +15,9 @@ BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 
-    EXECUTE ' INSERT INTO crowdmap_basic (the_geom, description, name)
-            VALUES ($1, $2, $3)
-            ' USING _the_geom, _description, _name;
+    EXECUTE ' INSERT INTO bikeways (the_geom, notes, name, zipcode)
+            VALUES ($1, $2, $3, $4)
+            ' USING _the_geom, _notes, _name, _zip;
             
     RETURN 1;
 END;
@@ -24,4 +25,4 @@ $$
 LANGUAGE plpgsql SECURITY DEFINER ;
 
 --Grant access to the public user
-GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data( text, text, text) TO publicuser;
+GRANT EXECUTE ON FUNCTION insert_bikeways_data(text,text,text,numeric) TO publicuser;
