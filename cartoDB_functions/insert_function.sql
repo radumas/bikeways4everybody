dc4fc1d9ac00d8d3eeb477c8d274f943ce633948
raphbcu <raph@bostoncyclistsunion.org>
2016-02-08 17:23:32 -0500

    Added email column

--- cartoDB_functions/insert_function.sql
+++ cartoDB_functions/insert_function.sql
@@ -5,7 +5,8 @@ CREATE OR REPLACE FUNCTION insert_bikeways_data (
     _geojson TEXT,
     _notes TEXT,
     _name TEXT,
-    _zip TEXT)
+    _zip TEXT,
+    _email TEXT)
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
@@ -15,9 +16,9 @@ BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 
-    EXECUTE ' INSERT INTO bikeways (the_geom, notes, name, zipcode)
-            VALUES ($1, $2, $3, $4)
-            ' USING _the_geom, _notes, _name, _zip;
+    EXECUTE ' INSERT INTO bikeways (the_geom, notes, name, zipcode, email)
+            VALUES ($1, $2, $3, $4, $5)
+            ' USING _the_geom, _notes, _name, _zip, _email;
             
     RETURN 1;
 END;
