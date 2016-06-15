25e6b961bbf5535c1b350d5e54790c2be0c20a8c
rad <raphael.a.dumas@gmail.com>
2016-06-15 13:48:57 -0400

    Updating requests call

--- data-analysis/process-data-call.py
+++ data-analysis/process-data-call.py
@@ -2,4 +2,5 @@ import requests
 username = 'raphaeld'
 url = 'https://' + username + '.cartodb.com/api/v2/sql'
 sql = {'q':'SELECT process_data()'}
-res = requests.get(url, data=sql)
+res = requests.post(url, data=sql)
+print(res.text)
