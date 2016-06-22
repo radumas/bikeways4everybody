45a05c197258cc2fa79249aa165ff57aa4950300
rad <raphael.a.dumas@gmail.com>
2016-06-22 12:30:22 -0400

    Updating with better logging practices

--- data-analysis/process-data-call.py
+++ data-analysis/process-data-call.py
@@ -1,6 +1,16 @@
 import requests
+import logging
+import json
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 username = 'raphaeld'
 url = 'https://' + username + '.cartodb.com/api/v2/sql'
-sql = {'q':'SELECT process_data()'}
+sql = {'q':'SELECT process_data();'}
+logger.info('Pinging {url} with query: {sql}'.format(url=url,sql=sql['q']))
 res = requests.post(url, data=sql)
-print(res.text)
+resj = res.json()
+errors = resj.get('error', 'No errors')
+if errors == 'No errors':  
+    logger.info('Query completed in: %s', resj.get('time'))
+else:
+    logger.warning('Error: %s', errors)
